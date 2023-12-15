2fb65a60016b19e654dee702bbc6ecee1d26227d
HNY_Badger <markelov-02@list.ru>
2023-12-15 23:04:39 +0400

    feat: finished binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,39 +8,155 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  head = null;
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (!this.head) {
+      this.head = data ? new Node(data) : new Node(null);
+      return
+    }
+    let parent = this.head;
+    while (true) {
+      if (data < parent.data) {
+        if (!parent.left) {
+          parent.left = data ? new Node(data) : new Node(null);
+          return;
+        } else {
+          parent = parent.left;
+          continue;
+        }
+      } else {
+        if (!parent.right) {
+          parent.right = data ? new Node(data) : new Node(null);
+          return;
+        } else {
+          parent = parent.right;
+          continue;
+        }
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.find(data) ? true : false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let parent = this.head;
+    while (true) {
+      if (parent === null) {
+        return null;
+      }
+      if (data === parent.data) {
+        return parent;
+      }
+      if (data < parent.data) {
+        if (parent.left) {
+          parent = parent.left;
+          continue;
+        } else {
+          return null;
+        }
+      } else {
+        if (parent.right) {
+          parent = parent.right;
+          continue;
+        } else {
+          return null;
+        }
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    let parent = null;
+    let node = this.head;
+    while (true) {
+      if (node === null) {
+        return;
+      }
+      if (data === node.data) {
+        if (node.left === null && node.right === null) {
+          if (parent === null) {
+            this.head = null;
+          } else {
+            this.swap(parent, node, null);
+          }
+        } else if (node.left !== null && node.right === null) {
+          this.swap(parent, node, node.left);
+        } else if (node.left === null && node.right !== null) {
+          this.swap(parent, node, node.right);
+        } else {
+          let currentParent = node;
+          let current = node.right;
+          while (true) {
+            if (current.left !== null) {
+              currentParent = current;
+              current = current.left;
+            } else {
+              this.swap(currentParent, current, current.right);
+              current.left = node.left;
+              current.right = node.right;
+              this.swap(parent, node, current);
+              break;
+            }
+          }
+        }
+        return;
+      }
+      parent = node;
+      if (data < node.data) {
+        if (node.left) {
+          node = node.left;
+          continue;
+        } else {
+          return;
+        }
+      } else {
+        if (node.right) {
+          node = node.right;
+          continue;
+        } else {
+          return;
+        }
+      }
+    }
+  }
+
+  swap(parent, node, to) {
+    if (parent === null) {
+      this.head = to;
+    } else if (parent.left === node) {
+      parent.left = to;
+    } else {
+      parent.right = to;
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.head === null) {
+      return 0;
+    }
+    let node = this.head;
+    while (node.left !== null) {
+      node = node.left;
+    }
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.head === null) {
+      return 0;
+    }
+    let node = this.head;
+    while (node.right !== null) {
+      node = node.right;
+    }
+    return node.data;
   }
 }
 
